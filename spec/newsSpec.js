7caaf0fcd679ce312bcc8b8c380d477050e00a2b
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-04 16:04:28 +0100

    Written first failing tests

---
+++ spec/newsSpec.js
@@ -0,0 +1,26 @@
+(function () {
+  let news = new News();
+
+  function newsObjectStartsWithEmptyTopStoriesArray(){
+    if(news.allArticles.length !== 0){
+      throw new Error('News object should start with empty top stories array by default')
+    }
+  };
+
+  function newsObjectStartsWithEmptyArticlesArray(){
+    if(news.allArticles.length !== 0){
+      throw new Error('News object should start with empty articles array by default')
+    }
+  };
+
+  function newsObjectStartsWithEmptySummaryString(){
+    if(news.summary === ""){
+      throw new Error('News object should start with empty summary string by default')
+    }
+  };
+
+  newsObjectStartsWithEmptyTopStoriesArray();
+  newsObjectStartsWithEmptyArticlesArray();
+  newsObjectStartsWithEmptySummaryString();
+
+})(this)
