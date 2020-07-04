2e68398c2625d8b2c3612f62d1bfdfcb126cf423
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-04 16:38:00 +0100

    Tests pass: retrieveData function makes api request

--- src/news.js
+++ src/news.js
@@ -4,4 +4,23 @@ class News {
     this.allArticles = []
     this.summary = ""
   }
+
+  retrieveData(apiRequestUrl){
+    let self = this;
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', apiRequestUrl, true)
+
+    request.onload = function() {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+      self.topStories = data.response.results
+    }
+
+    // Send request
+    request.send()
+  }
+
 }
