317e1b73bc0756f45820eaeaf7ae1a250347929e
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-04 16:37:11 +0100

    Amended test to account for synchronicity of api request

--- spec/newsSpec.js
+++ spec/newsSpec.js
@@ -14,8 +14,9 @@
   };
 
   function retrieveDataFunctionReturnsArrayOfTenArticles(){
-    news.retrieveData();
-    expect(news.topStories.length).toEqual(10)
+    apiRequestUrl = 'http://content.guardianapis.com/search?from-date=2020-07-04&show-fields=thumbnail&api-key=test'
+    news.retrieveData(apiRequestUrl);
+    setTimeout(function(){ expect(news.topStories.length).toEqual(10); }, 1000);
   };
 
   newsObjectStartsWithEmptyTopStoriesArray();
