761cb1ccf898aa18f098724deaf8bde6dda09466
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-05 09:21:47 +0100

    Linked to index.html file and changed folder structure

---
+++ public/src/news.js
@@ -0,0 +1,50 @@
+class News {
+  constructor(){
+    this.topStories = []
+    this.article = []
+    this.summary = ""
+  }
+
+  retrieveTopStories(apiRequestUrl){
+    let self = this;
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', apiRequestUrl, true)
+
+    request.onload = function() {
+      // Begin accessing JSON data here
+      var data = JSON.parse(this.response)
+      self.topStories = data.response.results
+      console.log(self.topStories)
+    }
+
+    // Send request
+    request.send()
+  }
+
+    retrieveArticleID(){
+      let self = this;
+      let id = window.location.hash.split('#article/')[1]
+      self.retrieveArticleSummary(id)
+    }
+
+    retrieveArticleSummary(id){
+    let self = this;
+
+    self.article = self.topStories[id]
+
+    let apiRequestUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + self.topStories[id].webUrl
+
+    var request = new XMLHttpRequest()
+    request.open('GET', apiRequestUrl, true)
+
+    request.onload = function() {
+      var data = JSON.parse(this.response)
+      self.summary = data
+    }
+    request.send()
+  }
+
+}
