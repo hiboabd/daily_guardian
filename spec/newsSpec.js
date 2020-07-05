7fdb6e836115e243df967d0acf9dec5fb83c1b6c
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-05 09:20:33 +0100

    Added test for retrieve article summary function

--- spec/newsSpec.js
+++ spec/newsSpec.js
@@ -5,23 +5,33 @@
     expect(news.topStories.length).toEqual(0)
   };
 
-  function newsObjectStartsWithEmptyArticlesArray(){
-    expect(news.allArticles.length).toEqual(0)
+  function newsObjectStartsWithEmptyArticleArray(){
+    expect(news.article.length).toEqual(0)
   };
 
   function newsObjectStartsWithEmptySummaryString(){
     expect(news.summary).toEqual("")
   };
 
-  function retrieveDataFunctionReturnsArrayOfTenArticles(){
+  function retrieveTopStoriesFunctionReturnsArrayOfTenArticles(){
     apiRequestUrl = 'http://content.guardianapis.com/search?from-date=2020-07-04&show-fields=thumbnail&api-key=test'
     news.retrieveTopStories(apiRequestUrl);
     setTimeout(function(){ expect(news.topStories.length).toEqual(10); }, 1000);
   };
 
+  function retrieveArticleSummaryFunctionReturnsArticleSummaryInfo(){
+    article = {
+      webUrl: "https://www.theguardian.com/stage/2020/mar/17/hottest-front-room-seats-the-best-theatre-and-dance-to-watch-online"
+    }
+    news.topStories = [article]
+    news.retrieveArticleSummary(0);
+    setTimeout(function(){ expect(news.summary).toNotEqual(""); }, 3000);
+  };
+
   newsObjectStartsWithEmptyTopStoriesArray();
-  newsObjectStartsWithEmptyArticlesArray();
+  newsObjectStartsWithEmptyArticleArray();
   newsObjectStartsWithEmptySummaryString();
   retrieveDataFunctionReturnsArrayOfTenArticles();
+  retrieveArticleSummaryFunctionReturnsArticleSummaryInfo();
 
 })(this)
