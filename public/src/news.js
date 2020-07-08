12e4443c261efe88dcf6fb5e6820202f353c822e
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-08 12:16:30 +0100

    Changed requests to be https not http

--- public/src/news.js
+++ public/src/news.js
@@ -36,7 +36,7 @@ class News {
 
       self.singleArticle = self.topStories[id]
 
-      let apiRequestUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + self.topStories[id].webUrl
+      let apiRequestUrl = 'https://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=' + self.topStories[id].webUrl
 
       var request = new XMLHttpRequest()
       request.open('GET', apiRequestUrl, true)
